bbefbc5ba83ff716b2e134b5e30916df4d8fc183
Sam Smith <13868670+SamHSmith@users.noreply.github.com>
2020-03-22 15:47:49 +0100

    setup

---
+++ CMakeFiles/VTrace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VTrace.dir/src/main.cpp.o"
+  "VTrace"
+  "VTrace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VTrace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
